175ebe97564e58e8d529fe2043e3d2dae0273303
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2017-06-15 15:08:51 -0700

    FIX: can now include externally

--- packages/art-epoched-state/index.coffee
+++ packages/art-epoched-state/index.coffee
@@ -1,1 +1,1 @@
-module.exports = require './source/Art'
+module.exports = require('./source/Art').Epoch
--- packages/art-epoched-state/index.js
+++ packages/art-epoched-state/index.js
@@ -2,5 +2,5 @@ if (false) { // use build? - true == fase, false == good for development
   module.exports = require('./build');
 } else {
   require('./register');
-  module.exports = require('./index.caf');
+  module.exports = require('./index.coffee');
 };
