f6d88f9d0eaf792c12e95d276fb94706ab6666e0
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-01-02 12:00:56 -0800

    BUILD

--- packages/art-config/build/index.js
+++ packages/art-config/build/index.js
@@ -310,15 +310,15 @@ defineModule(module, Main = (function() {
 
   function Main() {}
 
-  getArtConfigName = function() {
+  Main.getArtConfigName = getArtConfigName = function() {
     return Neptune.Art.Config.configName;
   };
 
-  getArtConfig = function() {
+  Main.getArtConfig = getArtConfig = function() {
     return Neptune.Art.Config.config;
   };
 
-  getDefaultArtConfigName = function() {
+  Main.getDefaultArtConfigName = getDefaultArtConfigName = function() {
     return Neptune.Art.Config.defaultArtConfigName;
   };
 
@@ -638,6 +638,10 @@ defineModule(module, Configurable = (function(superClass) {
     defaults: {}
   });
 
+  Configurable.getDefaultConfig = function() {
+    return this.getDefaults();
+  };
+
   Configurable.extendConfig = function() {
     if (this.hasOwnProperty("config")) {
       return this.config;
@@ -753,7 +757,7 @@ defineModule(module, Configurable = (function(superClass) {
 /* 12 */
 /***/ (function(module, exports) {
 
-module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-events":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-script":"*","caffeine-script-runtime":"*","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.1","glob":"^7.1.2","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"A powerful yet simple tool for configuring all your libraries consistently.","license":"ISC","name":"art-config","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"1.8.4"}
+module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*","art-class-system":"*","art-config":"*","art-events":"*","art-standard-lib":"*","art-testbench":"*","bluebird":"^3.5.0","caffeine-script":"*","caffeine-script-runtime":"*","case-sensitive-paths-webpack-plugin":"^2.1.1","chai":"^4.0.1","coffee-loader":"^0.7.3","coffee-script":"^1.12.6","colors":"^1.1.2","commander":"^2.9.0","css-loader":"^0.28.4","dateformat":"^2.0.0","detect-node":"^2.0.3","fs-extra":"^3.0.1","glob":"^7.1.2","glob-promise":"^3.1.0","json-loader":"^0.5.4","mocha":"^3.4.2","neptune-namespaces":"*","script-loader":"^0.7.0","style-loader":"^0.18.1","webpack":"^2.6.1","webpack-dev-server":"^2.4.5","webpack-merge":"^4.1.0","webpack-node-externals":"^1.6.0"},"description":"A powerful yet simple tool for configuring all your libraries consistently.","license":"ISC","name":"art-config","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd --compilers coffee:coffee-script/register","testInBrowser":"webpack-dev-server --progress"},"version":"1.8.6"}
 
 /***/ }),
 /* 13 */
