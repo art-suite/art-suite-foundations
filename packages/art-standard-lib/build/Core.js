d1b04bcb9bb10d6032fc2d87fdeb5ea81b681df1
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-07-27 10:36:30 -0700

    BULID

--- packages/art-standard-lib/build/Core.js
+++ packages/art-standard-lib/build/Core.js
@@ -842,7 +842,7 @@ module.exports = require('neptune-namespaces' /* ABC - not inlining fellow NPM *
 /*! exports provided: author, dependencies, description, license, name, scripts, version, default */
 /***/ (function(module) {
 
-module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*"},"description":"The Standard Library for JavaScript that aught to be.","license":"ISC","name":"art-standard-lib","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd","testInBrowser":"webpack-dev-server --progress"},"version":"1.42.3"};
+module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*"},"description":"The Standard Library for JavaScript that aught to be.","license":"ISC","name":"art-standard-lib","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd","testInBrowser":"webpack-dev-server --progress"},"version":"1.43.0"};
 
 /***/ }),
 
--- packages/art-standard-lib/build/index.js
+++ packages/art-standard-lib/build/index.js
@@ -195,7 +195,7 @@ module.exports = require('neptune-namespaces' /* ABC - not inlining fellow NPM *
 /*! exports provided: author, dependencies, description, license, name, scripts, version, default */
 /***/ (function(module) {
 
-module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*"},"description":"The Standard Library for JavaScript that aught to be.","license":"ISC","name":"art-standard-lib","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd","testInBrowser":"webpack-dev-server --progress"},"version":"1.42.3"};
+module.exports = {"author":"Shane Brinkman-Davis Delamore, Imikimi LLC","dependencies":{"art-build-configurator":"*"},"description":"The Standard Library for JavaScript that aught to be.","license":"ISC","name":"art-standard-lib","scripts":{"build":"webpack --progress","start":"webpack-dev-server --hot --inline --progress","test":"nn -s;mocha -u tdd","testInBrowser":"webpack-dev-server --progress"},"version":"1.43.0"};
 
 /***/ }),
 /* 5 */
@@ -2325,7 +2325,7 @@ module.exports = Inspector = (function() {
     if (options == null) {
       options = {};
     }
-    if (this !== global) {
+    if (this && this !== global) {
       return Neptune.Base.inspect.call(this);
     }
     inspector = new Inspector(options);
@@ -7180,8 +7180,35 @@ module.exports = Time = (function() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
 
 /*
+2018-07-27 SBD Notes
+
+I want to move towards:
+
+  clone: is a combination of stuctural clone and calling .clone, if available. Otherwise, it doesn't do what I'm calling invasive-cloning.
+    invasiveCloning: false
+    customCloning: true
+    recusiveSafe: fully
+    Note: ArtAtomic objects have .clones that return new objects, however, ArtAtomic objects are designed to be used in a read-only way,
+      so this is a waste most of the time. UNLESS you intend to modify the cloned object, which sometimes you do need to do.
+      So, that's why cloneStructure is often the right answer - it won't clone non-plain-objects
+
+  cloneStructure: clone only plain-object and arrays. Everything else is just a simple assignment.
+    invasiveCloning: false
+    customCloning: false
+    recusiveSafe: stack - (See below)
+
+  invasiveClone:
+    invasiveCloning: false
+    customCloning: false
+    recusiveSafe: full
+    (cloneByProperties basically did this)
+    Deeply clone everything, manually cloning objects regardless if they have a .clone method.
+
+---
 This current iteration of clone relies on some singleton variables shared across all invocations of clone.
 This is fine as long as javascript stays single-threaded.
 It also introduces a little bit of uglyness initializing clonedMap necessitating the "top" variable.
@@ -7192,13 +7219,15 @@ but when recursing, pass in a new function bound to that closure which is differ
 
 populateClone would need to take an additional argument - the clone function to use for recursive cloning.
  */
-var Clone, Map, Unique, byProperties, byStructure, clonedMap, inspect, topObject, uniquePropertyName;
+var Clone, Map, Unique, byProperties, byStructure, clonedMap, inspect, isArray, isPlainObject, ref, topObject, uniquePropertyName;
 
 Map = __webpack_require__(/*! ./Map */ 28);
 
 Unique = __webpack_require__(/*! ./Unique */ 29);
 
 inspect = __webpack_require__(/*! ./Inspect */ 25).inspect;
+
+ref = __webpack_require__(/*! ./Core/Types */ 14), isPlainObject = ref.isPlainObject, isArray = ref.isArray;
 
 uniquePropertyName = Unique.PropertyName;
 
@@ -7211,16 +7240,16 @@ byProperties = false;
 topObject = null;
 
 module.exports = Clone = (function() {
-  var clone, cloneArray, cloneByProperties, cloneByStructure, cloneObject, emptyClone;
+  var _clone, _cloneStructure, cloneArray, cloneByProperties, cloneByStructure, cloneObject, cloneStructure, cloneStructureFromStack, cloneStructurePush, cloneStructureToStack, cloneSturcturePop, emptyClone, isStructural;
 
   function Clone() {}
 
   cloneArray = function(array) {
-    var clonedArray, i, index, len, value;
+    var clonedArray, index, j, len, value;
     clonedMap.set(array, clonedArray = array.slice());
-    for (index = i = 0, len = clonedArray.length; i < len; index = ++i) {
+    for (index = j = 0, len = clonedArray.length; j < len; index = ++j) {
       value = clonedArray[index];
-      clonedArray[index] = clone(value);
+      clonedArray[index] = _clone(value);
     }
     return clonedArray;
   };
@@ -7233,21 +7262,21 @@ module.exports = Clone = (function() {
     } else {
       for (k in obj) {
         v = obj[k];
-        clonedObject[k] = clone(v);
+        clonedObject[k] = _clone(v);
       }
     }
     return clonedObject;
   };
 
   Clone.emptyClone = emptyClone = function(obj) {
-    if (obj.constructor === Array) {
+    if (isArray(obj)) {
       return [];
     } else {
       return Object.create(Object.getPrototypeOf(obj));
     }
   };
 
-  Clone.clone = clone = function(obj, mode) {
+  Clone._clone = _clone = function(obj, mode) {
     var clonedObject, got;
     switch (mode) {
       case "byStructure":
@@ -7259,7 +7288,7 @@ module.exports = Clone = (function() {
     if (obj === null || obj === void 0 || typeof obj !== "object") {
       return obj;
     }
-    if (byStructure && (obj.constructor !== Array && obj.constructor !== Object)) {
+    if (byStructure && !(isArray(obj || isPlainObject(obj)))) {
       return obj;
     }
     if (clonedMap) {
@@ -7270,7 +7299,7 @@ module.exports = Clone = (function() {
       topObject = obj;
       clonedMap = new Map;
     }
-    clonedObject = obj.constructor === Array ? cloneArray(obj) : cloneObject(obj);
+    clonedObject = isArray(obj) ? cloneArray(obj) : cloneObject(obj);
     if (topObject === obj) {
       byStructure = false;
       byProperties = false;
@@ -7280,12 +7309,101 @@ module.exports = Clone = (function() {
     return clonedObject;
   };
 
+  Clone.clone = function(obj, mode) {
+    if (mode != null) {
+      console.error("2018-07-27: clone mode-param is DEPRICATED. Partial solution, see: cloneStructure");
+    }
+    return _clone(obj, mode);
+  };
+
   Clone.cloneByProperties = cloneByProperties = function(obj) {
-    return clone(obj, "byProperties");
+    console.error("2018-07-27: cloneByProperties is DEPRICATED. Partial solution, see: cloneStructure");
+    return _clone(obj, "byProperties");
   };
 
   Clone.cloneByStructure = cloneByStructure = function(obj) {
-    return clone(obj, "byStructure");
+    console.error("2018-07-27: cloneByStructure is DEPRICATED. Use: cloneStructure");
+    return _clone(obj, "byStructure");
+  };
+
+  Clone.prototype.isStructural = isStructural = function(obj) {
+    return isPlainObject(obj) || isArray(obj);
+  };
+
+
+  /*
+  clones plain objects and arrays, but not any other type
+  
+  FEATURES
+    - no allocations beyond the newly crearted object and arrays
+  
+  recursiveSafe: uses a stack
+    This means:
+      NO two objects or arrays in the output structure will be "==="
+      UNLESS they were in a (grand)parent/(grand)child relationship in the source.
+  
+      This means if you have the same (===) object/array more than once in the structure,
+      in a non (grand)parent/(grand)child way, each use will get a separate cloned output
+      in the new structure. In other words
+  
+    This as a nice advantage: the output is JSON-compatible.
+  
+    This is mostly a performance optimization. It allows us to avoid any extra object allocations.
+    Once we can safely use the new ES6 Map everywhere, we might perf-test again to see if full
+    recursion-safety isn't just as fast.
+    But then we lose JSON-compatible output guarantees...
+   */
+
+  Clone.cloneStructure = cloneStructure = function(inValue) {
+    var cloningStructurePushed, isA;
+    cloningStructurePushed = false;
+    if (isPlainObject(inValue) || (isA = isArray(inValue))) {
+      return _cloneStructure(inValue, isA);
+    } else {
+      return inValue;
+    }
+  };
+
+  cloneStructureFromStack = [];
+
+  cloneStructureToStack = [];
+
+  cloneStructurePush = function(inValue, outValue) {
+    cloneStructureFromStack.push(inValue);
+    cloneStructureToStack.push(outValue);
+    return true;
+  };
+
+  cloneSturcturePop = function() {
+    cloneStructureFromStack.pop();
+    return cloneStructureToStack.pop();
+  };
+
+  _cloneStructure = function(inObjOrArray, inputIsArray) {
+    var i, j, k, len, outValue, pushed, v, vIsArray;
+    if (0 <= (i = cloneStructureFromStack != null ? cloneStructureFromStack.indexOf(inObjOrArray) : void 0)) {
+      return cloneStructureToStack[i];
+    } else {
+      pushed = false;
+      outValue = null;
+      if (inputIsArray) {
+        outValue = [];
+        for (j = 0, len = inObjOrArray.length; j < len; j++) {
+          v = inObjOrArray[j];
+          outValue.push(isPlainObject(v) || (vIsArray = isArray(v)) ? (pushed || (pushed = cloneStructurePush(inObjOrArray, outValue)), _cloneStructure(v, vIsArray)) : v);
+        }
+      } else {
+        outValue = {};
+        for (k in inObjOrArray) {
+          v = inObjOrArray[k];
+          outValue[k] = isPlainObject(v) || (vIsArray = isArray(v)) ? (pushed || (pushed = cloneStructurePush(inObjOrArray, outValue)), _cloneStructure(v, vIsArray)) : v;
+        }
+      }
+      if (pushed) {
+        cloneSturcturePop();
+      }
+      return outValue;
+    }
   };
 
   return Clone;
