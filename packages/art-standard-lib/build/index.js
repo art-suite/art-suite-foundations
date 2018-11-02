78dff96712f647ea163e60275d0b9b8c41a9d89f
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-11-02 09:49:43 -0700

    NEW: hex16GreyColorRegExp hex256GreyColorRegExp

--- packages/art-standard-lib/build/index.js
+++ packages/art-standard-lib/build/index.js
@@ -2183,6 +2183,10 @@ module.exports = RegExpExtensions = (function() {
 
   RegExpExtensions.isoDateRegExp = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
 
+  RegExpExtensions.hex16GreyColorRegExp = /^#([a-f0-9])$/i;
+
+  RegExpExtensions.hex256GreyColorRegExp = /^#([a-f0-9]{2})$/i;
+
   RegExpExtensions.hex16ColorRegExp = /^#([a-f0-9])([a-f0-9])([a-f0-9])([a-f0-9])?$/i;
 
   RegExpExtensions.hex256ColorRegExp = /^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})?$/i;
