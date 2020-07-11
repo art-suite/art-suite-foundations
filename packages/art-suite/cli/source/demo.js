dacacbe42a8663ae0481ad4e28fe18f6747d749f
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-07-11 13:01:11 -0700

    patch: demo updated with new help+commands format

--- packages/art-suite/cli/source/demo.js
+++ packages/art-suite/cli/source/demo.js
@@ -7,18 +7,42 @@ Caf.defMod(module, () => {
     (compactFlatten, repeat, Object) => {
       return require("./ArtSuite.Cli").start({
         commands: {
-          sing: function({ song }) {
-            return `♫ ${Caf.toString(song)} ♫!`;
+          sing: {
+            action: function({ song }) {
+              return `♫ ${Caf.toString(song)} ♫!`;
+            },
+            description: "Sing any name you choose",
+            options: {
+              song: { description: "name of the song to sing", required: true }
+            }
           },
           "send-cheer": function() {
             return "May you have a holly, jolly Christmas this year!";
           },
-          xmas: function({ santa, rudolph }) {
-            return compactFlatten([
-              "Will Santa come this year?",
-              santa > 0 ? repeat("Ho! ", santa) : "No Santa this year.",
-              rudolph ? "Rudolph's nose glows!" : undefined
-            ]).join("\n");
+          xmas: {
+            action: function({ santa, rudolph }) {
+              return compactFlatten([
+                "Will Santa come this year?",
+                santa > 0 ? repeat("Ho! ", santa) : "No Santa this year.",
+                rudolph ? "Rudolph's nose glows!" : undefined
+              ]).join("\n");
+            },
+            description: "This will tell you exactly what you need.",
+            options: {
+              rudolph: "Should Rudolph come too?",
+              santa: [
+                "number-of-ho-hos",
+                "Santa will come if he says 'Ho!' at least once."
+              ]
+            },
+            examples: [
+              { santa: 10 },
+              "output:\n\n  Will Santa come this year?\n  Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho!",
+              { rudolph: true },
+              "output:\n\n  Will Santa come this year?\n  No Santa this year.\n  Rudolph's nose glows!",
+              { rudolph: true, santa: 10 },
+              "output:\n\n  Will Santa come this year?\n  Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho!\n  Rudolph's nose glows!"
+            ]
           },
           stat: {
             description: "stat the listed files",
@@ -34,39 +58,6 @@ Caf.defMod(module, () => {
                   k => k
                 );
               });
-            }
-          }
-        },
-        help: {
-          description: "A demo app with a holiday theme.",
-          commands: {
-            sing: {
-              description: "Sing any name you choose",
-              options: {
-                song: {
-                  description: "name of the song to sing",
-                  required: true
-                }
-              }
-            },
-            "send-cheer": { description: "Just a little holiday cheer." },
-            xmas: {
-              description: "This will tell you exactly what you need.",
-              options: {
-                rudolph: "Should Rudolph come too?",
-                santa: [
-                  "number-of-ho-hos",
-                  "Santa will come if he says 'Ho!' at least once."
-                ]
-              },
-              examples: [
-                { santa: 10 },
-                "output:\n\n  Will Santa come this year?\n  Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho!",
-                { rudolph: true },
-                "output:\n\n  Will Santa come this year?\n  No Santa this year.\n  Rudolph's nose glows!",
-                { rudolph: true, santa: 10 },
-                "output:\n\n  Will Santa come this year?\n  Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho!\n  Rudolph's nose glows!"
-              ]
             }
           }
         }
