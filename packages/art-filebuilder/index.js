a77805c8c4624ea86ed6417e625e408fb4008222
shanebdavis <shanebdavis@gmail.com>
2020-08-22 16:26:21 -0700

    patch/refactor: refactor to use source-source

--- packages/art-filebuilder/index.js
+++ packages/art-filebuilder/index.js
@@ -1,6 +1,1 @@
-if (require('./use-build')) { // use build? - true == fase, false == good for development
-  module.exports = require('./build');
-} else {
-  require('./register');
-  module.exports = require('./index.caf');
-};
+require("./source");
