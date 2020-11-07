1059fba8eaee6b984fabaf1740a2769340375a67
shanebdavis <shanebdavis@gmail.com>
2020-11-07 15:41:18 -0800

    major/change: CHANGE "assert.is" is now the same as CaffeineScript's "a is b"; refactored more code into Caf

---
+++ packages/art-testbench/source/test/assert.exists.test.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return Caf.importInvoke(["pairedAssertTests"], [global, require('./StandardImport')], (pairedAssertTests) => {return pairedAssertTests("exists", "doesNotExist", [[true, ""], [true, 0], [false, null], [false, undefined]]);});});
+//# sourceMappingURL=assert.exists.test.js.map
