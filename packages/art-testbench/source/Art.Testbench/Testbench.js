8f5a9bfcd0adda479be53619f3f7c9c644b11c55
shanebdavis <shanebdavis@gmail.com>
2020-11-09 15:58:23 -0800

    patch/fix: now that we have @art-suite/chained-test and /assert, we can remove them from art-testbench

--- packages/art-testbench/source/Art.Testbench/Testbench.js
+++ packages/art-testbench/source/Art.Testbench/Testbench.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let test, setup, initTesting; return require('art-standard-lib').merge(require('./Jest'), ({test, setup, initTesting} = require('./Mocha'), {test, setup, initTesting}), {chainedTest: require('./ChainedTest').chainedTest, assert: require('./Assert').assert});});
+Caf.defMod(module, () => {let test, setup, initTesting; return require('art-standard-lib').merge(require('./Jest'), ({test, setup, initTesting} = require('./Mocha'), {test, setup, initTesting}), {chainedTest: require("@art-suite/chained-test").chainedTest, assert: require('./Assert').assert});});
 //# sourceMappingURL=Testbench.js.map
