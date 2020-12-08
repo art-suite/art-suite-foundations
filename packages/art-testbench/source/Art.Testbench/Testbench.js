0fc383e39e4f5fbf03d6fb42d8b84dc973371dbb
shanebdavis <shanebdavis@gmail.com>
2020-12-08 08:57:38 -0800

    patch/fix: FIX - global assert is re-supported - for now; didn't mean to make that breaking change

--- packages/art-testbench/source/Art.Testbench/Testbench.js
+++ packages/art-testbench/source/Art.Testbench/Testbench.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let setup, initTesting; return [require('art-standard-lib'), require('./JestExtensions'), ({setup, initTesting} = require('./Mocha'), {setup, initTesting}), require('./Test'), {chainedTest: require('@art-suite/chained-test').chainedTest, assert: require('@art-suite/assert').assert}];});
+Caf.defMod(module, () => {let setup, initTesting; return [require('art-standard-lib'), require('./JestExtensions'), ({setup, initTesting} = require('./Mocha'), {setup, initTesting}), require('./Test'), {chainedTest: require('@art-suite/chained-test').chainedTest, assert: global.assert = require('@art-suite/assert').assert}];});
 //# sourceMappingURL=Testbench.js.map
