f6dcff3806c9d5bae3d3d0142787ba4b8afa2c81
shanebdavis <shanebdavis@gmail.com>
2021-01-03 10:39:47 -0800

    patch/test: fixed tests

--- packages/@art-suite/cli/source/index.js
+++ packages/@art-suite/cli/source/index.js
@@ -1,5 +1,4 @@
-"use strict";
-let Caf = require("caffeine-script-runtime");
-Caf.defMod(module, () => {
-  return require("./ArtSuite.Cli");
-});
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return require('./ArtSuite.Cli');});
+//# sourceMappingURL=index.js.map
