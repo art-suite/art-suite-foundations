d44ab5d5e718b7bd638bf6c95eb71ec1ef003927
shanebdavis <shanebdavis@gmail.com>
2021-07-28 11:33:18 -0700

    patch/refactor: art-config updated to new standard-package-build

---
+++ packages/art-config/build/test/Art.Config/StandardImport.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('../../../build')];});
+//# sourceMappingURL=StandardImport.js.map
