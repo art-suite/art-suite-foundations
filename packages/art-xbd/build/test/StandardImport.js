52c1c514ee561a16c2a26e94ef81daefd2411800
shanebdavis <shanebdavis@gmail.com>
2021-07-28 15:14:00 -0700

    v1.1.6

--- packages/art-xbd/build/test/StandardImport.js
+++ packages/art-xbd/build/test/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('art-standard-lib').mergeWithSelf(require('../Art.Xbd'), require('art-testbench'));});
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('../Art.Xbd'), require('art-testbench')];});
 //# sourceMappingURL=StandardImport.js.map
