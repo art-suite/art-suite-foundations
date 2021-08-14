a72523c8b7b8cd5ce8081b60c6854b329204706e
shanebdavis <shanebdavis@gmail.com>
2021-08-13 19:31:10 -0700

    patch/deps: more restoring old, factored-out packages

--- packages/@art-suite/art-foundation/build/Art.Foundation/Foundation.js
+++ packages/@art-suite/art-foundation/build/Art.Foundation/Foundation.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-class-system'), require('art-config'), require('art-binary'), require('art-validation'), require('./ForHumans'), require('./Tools'), {CommunicationStatus: require('art-communication-status'), Epoch: require('art-epoched-state').EpochClass, Browser: require('art-browser-tools'), Binary: require('art-binary')}];});
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-class-system'), require('art-config'), require('art-binary'), require('art-validation'), require('art-object-tree-factory'), require('art-communication-status'), require('art-rest-client'), require('@art-suite/web-worker'), require('@art-suite/local-storage'), require('./ForHumans'), require('./Tools'), {RestClient: require('art-rest-client'), CommunicationStatus: require('art-communication-status'), Epoch: require('art-epoched-state').EpochClass, Browser: require('art-browser-tools'), Binary: require('art-binary')}];});
 //# sourceMappingURL=Foundation.js.map
--- packages/@art-suite/art-foundation/build/Art.Foundation/Tools/Tools.js
+++ packages/@art-suite/art-foundation/build/Art.Foundation/Tools/Tools.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [[require('./ProgressAdapter'), "executePromiseSequence"], require('@art-suite/web-worker'), require('@art-suite/local-storage'), require('art-object-tree-factory'), require('./GlobalCounts'), require('art-communication-status'), require('art-rest-client'), require('art-validation').Validator];});
+Caf.defMod(module, () => {return [[require('./ProgressAdapter'), "executePromiseSequence"], require('./GlobalCounts')];});
 //# sourceMappingURL=Tools.js.map
