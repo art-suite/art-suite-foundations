4f193abb8667427ea4899435d3852bb451e658ec
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-02-04 09:04:31 +0100

    patch: remove build; fixed build-script copy-js to use rsync which is always available

--- packages/@art-suite/standard-package-builder/build/test/StandardImport.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-testbench'), {standardPackageBuilder: require('../')}];});
-//# sourceMappingURL=StandardImport.js.map
