0927487f14c3ec8f847c02ca846f5831ed545ef5
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-02-05 13:15:38 +0100

    patch/tooling: removed build and converage from repo for CLI

--- packages/@art-suite/cli/build/ArtSuite.Cli/Cli.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return {start: require('./Main').start};});
-//# sourceMappingURL=Cli.js.map
--- packages/@art-suite/cli/build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('./ArtSuite.Cli');});
-//# sourceMappingURL=index.js.map
