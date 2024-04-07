2e5d87db431b7917a24bb0168ebf7a3653e123b9
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-04-07 14:00:50 +0200

    patch/deps: completed glob@10 conversion

--- packages/neptune-namespaces/build/NeptuneNamespaces/StandardImport.js
+++ packages/neptune-namespaces/build/NeptuneNamespaces/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let out; out = {glob: require('glob').globSync ? require('glob').glob : require('glob-promise'), fsp: require('fs-extra'), Path: require('path')}; Caf.object(require('./Helper'), null, null, out); Caf.object(require('./MiniFoundation'), null, null, out); return out;});
+Caf.defMod(module, () => {let out; out = {glob: require('glob').glob, fsp: require('fs-extra'), Path: require('path')}; Caf.object(require('./Helper'), null, null, out); Caf.object(require('./MiniFoundation'), null, null, out); return out;});
 //# sourceMappingURL=StandardImport.js.map
