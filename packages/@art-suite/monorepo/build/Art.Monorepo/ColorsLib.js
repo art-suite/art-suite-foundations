194321f7da80bd0acf466ec2575260cff2ce90e3
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-05-30 11:14:56 +0200

    patch/deps: sync package.json

--- packages/@art-suite/monorepo/build/Art.Monorepo/ColorsLib.js
+++ packages/@art-suite/monorepo/build/Art.Monorepo/ColorsLib.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let blue, yellow, green, grey, red; return ({blue, yellow, green, grey, red} = require('chalk'), {blue, yellow, green, grey, red});});
+Caf.defMod(module, () => {let blue, yellow, green, grey, red; return ({blue, yellow, green, grey, red} = require('tiny-chalk'), {blue, yellow, green, grey, red});});
 //# sourceMappingURL=ColorsLib.js.map
