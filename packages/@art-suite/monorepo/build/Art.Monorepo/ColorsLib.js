52fb20dc5570941d2545244693bfb5e3074db9c8
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-05-30 15:21:24 +0200

    patch/fix: actiually using terminalColors in the build now

--- packages/@art-suite/monorepo/build/Art.Monorepo/ColorsLib.js
+++ packages/@art-suite/monorepo/build/Art.Monorepo/ColorsLib.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {let blue, yellow, green, grey, red; return ({blue, yellow, green, grey, red} = require('tiny-chalk'), {blue, yellow, green, grey, red});});
+Caf.defMod(module, () => {let blue, yellow, green, grey, red; return ({blue, yellow, green, grey, red} = require('art-standard-lib').terminalColors, {blue, yellow, green, grey, red});});
 //# sourceMappingURL=ColorsLib.js.map
