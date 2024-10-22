28a112945b90a7e98cbad208e418acfb4e4633bc
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-10-22 14:52:58 +0200

    patch/tooling: added awaited to types

--- packages/@art-suite/chained-test/index.d.ts
+++ packages/@art-suite/chained-test/index.d.ts
@@ -1,19 +1,20 @@
 declare module "@art-suite/chained-test" {
-  export function chainedTest<T>(name: string, fn: () => T): ChainedTest<T, T>;
-  export function firstIt<T>(name: string, fn: () => T): ChainedTest<T, T>;
+  export function chainedTest<T>(name: string, fn: () => T): ChainedTest<Awaited<T>, Awaited<T>>;
+  export function firstIt<T>(name: string, fn: () => T): ChainedTest<Awaited<T>, Awaited<T>>;
 
   export interface ChainedTest<SetupResult, ChainResult> {
-    thenTest<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, U>;
-    thenIt<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, U>;
-    alwaysTest<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
-    alwaysIt<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
-    finally<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
+    thenTest<NextChainedResult>(name: string, fn: (input: ChainResult) => NextChainedResult): ChainedTest<SetupResult, Awaited<NextChainedResult>>;
+    thenIt<NextChainedResult>(name: string, fn: (input: ChainResult) => NextChainedResult): ChainedTest<SetupResult, Awaited<NextChainedResult>>;
+    alwaysTest(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, SetupResult>;
+    alwaysIt(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, SetupResult>;
+    finally(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, SetupResult>;
 
     tapTest(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
     tapIt(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
 
     softTapTest(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
     softTapIt(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
-    thenTestMerged<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, ChainResult & U>;
+
+    thenTestMerged<NextChainedResult>(name: string, fn: (input: ChainResult) => NextChainedResult): ChainedTest<SetupResult, ChainResult & Awaited<NextChainedResult>>;
   }
 }
