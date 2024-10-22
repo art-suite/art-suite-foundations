d255516beccb5b9eb56a512df36d8c825115a34e
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2024-10-22 14:32:15 +0200

    patch/improvement: add typescript types

---
+++ packages/@art-suite/chained-test/index.d.ts
@@ -0,0 +1,18 @@
+declare module "@art-suite/chained-test" {
+  export function chainedTest<T>(name: string, fn: () => T): ChainedTest<T, T>;
+
+  export interface ChainedTest<SetupResult, ChainResult> {
+    thenTest<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, U>;
+    thenIt<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, U>;
+    alwaysTest<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
+    alwaysIt<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
+    finally<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, SetupResult>;
+
+    tapTest(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
+    tapIt(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
+
+    softTapTest(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
+    softTapIt(name: string, fn: (input: ChainResult) => any): ChainedTest<SetupResult, ChainResult>;
+    thenTestMerged<U>(name: string, fn: (input: ChainResult) => U): ChainedTest<SetupResult, ChainResult & U>;
+  }
+}
