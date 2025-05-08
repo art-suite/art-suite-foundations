1aaf5bae0b39be584c9c775928bebd79af68088e
Shane Delamore <shanebdavis@gmail.com>
2025-05-08 22:52:17 +0200

    patch/improvement: converted log to caffeinescript!

--- packages/@art-suite/art-core/art-core-logging/build/ArtCoreLogging/StandardImport.js
+++ packages/@art-suite/art-core/art-core-logging/build/ArtCoreLogging/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return [require('@art-suite/art-core-types'), require('@art-suite/art-core-time')];});
+Caf.defMod(module, () => {return [require('@art-suite/art-core-arrays'), require('@art-suite/art-core-environment'), require('@art-suite/art-core-objects'), require('@art-suite/art-core-promises'), require('@art-suite/art-core-terminal-colors'), require('@art-suite/art-core-time'), require('@art-suite/art-core-types')];});
 //# sourceMappingURL=StandardImport.js.map
