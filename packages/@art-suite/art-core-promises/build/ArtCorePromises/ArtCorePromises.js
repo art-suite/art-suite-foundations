c1ec8aa76e21594fb3cf70b4d4015dff5e8dad34
Shane Delamore <shanebdavis@gmail.com>
2025-05-08 12:53:30 +0200

    patch/improvement: breaking out art-standard-lib into art-core-*
    I'm removing and renaming rarely used functions; so this won't be a drop-in replacement for art-standard-lib (at least without running tests)

---
+++ packages/@art-suite/art-core-promises/build/ArtCorePromises/ArtCorePromises.js
@@ -0,0 +1,4 @@
+"use strict"
+let Caf = require('caffeine-script-runtime');
+Caf.defMod(module, () => {return [{PromiseWorkerPool: require('./PromiseWorkerPool')}, require('./ArtPromise'), require('./AsyncExtensions')];});
+//# sourceMappingURL=ArtCorePromises.js.map
