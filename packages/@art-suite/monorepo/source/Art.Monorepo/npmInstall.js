c3af3740c896fde4f7269241984b9f67d631bf4c
Shane Delamore <shanebdavis@gmail.com>
2025-07-27 10:32:12 -0800

    patch/improvement: better information during commands

--- packages/@art-suite/monorepo/source/Art.Monorepo/npmInstall.js
+++ packages/@art-suite/monorepo/source/Art.Monorepo/npmInstall.js
@@ -1,11 +1,14 @@
-const { spawn } = require('child_process')
+const { log } = require('art-standard-lib')
+const { yellow } = require('./ColorsLib')
 
 /**
  * Runs `npm install` inside an explicit shell
  * @param {string} cwd
  * @returns {Promise<void>}
  */
-const npmInstall = async () =>
-  require('child_process').execSync('npm install', { stdio: 'inherit' })
+const npmInstall = async () => {
+  log(yellow("Running `npm install` in the root..."))
+  return require('child_process').execSync('npm install', { stdio: 'inherit' })
+}
 
 module.exports = { npmInstall }
